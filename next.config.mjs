fb666bd44749074b19ee0de09c53f6f4258be223
deepsinghaman4748git <deepsingh.aman4748@gmail.com>
2026-08-19 23:11:18 +0530

    Remove local development origin from production config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-﻿/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: ['192.168.31.243'],
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
